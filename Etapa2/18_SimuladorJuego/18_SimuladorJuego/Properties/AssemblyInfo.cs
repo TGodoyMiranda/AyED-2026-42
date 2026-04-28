79d912d9395f0495a3c2077e4e7e05ea0d1fff12
Tiziano Godoy <tizianonicolasgodoymiranda@gmail.com>
2026-04-28 16:33:34 -0300

    TPs

--- Etapa2/18_SimuladorJuego/18_SimuladorJuego/Properties/AssemblyInfo.cs
+++ Etapa2/18_SimuladorJuego/18_SimuladorJuego/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("awawa")]
+[assembly: AssemblyTitle("18_SimuladorJuego")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("awawa")]
+[assembly: AssemblyProduct("18_SimuladorJuego")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6677c08b-e49d-4821-be6a-e84b6f59c886")]
+[assembly: Guid("e8a64926-2c23-406f-9b65-0530b174999a")]
 
 // Version information for an assembly consists of the following four values:
 //
