6b9616664db8eac17723da05ad2c2597500d0298
TGodoyMiranda <tizianonicolasgodoymiranda@gmail.com>
2026-04-29 17:14:10 -0300

    TPs

--- Etapa2/18_SimuladorJuego/18_SimuladorJuego/Program.cs
+++ Etapa2/18_SimuladorJuego/18_SimuladorJuego/Program.cs
@@ -13,37 +13,25 @@ namespace _18_SimuladorJuego
             int Vida = 10; int Hambre = 10; int Dias = 1;
             int Comida_Cruda = 0; int Comida_Cocida = 0;
             bool Refugio = false; bool Fogata = false;
-            bool FinPartida = false;
+            bool FinPartida = false; int Madera = 0;
             string Accion = "";
             while (FinPartida != true)
             {
+                if (Vida <= 0)
+                {
+                    Console.WriteLine("> Perdiste!");
+                    Console.ReadKey();
+                    return;
+                }
                 Random NumR = new Random();
-                NumR.Next(1, 101);
+                int Num = NumR.Next(1, 101);
                 Console.WriteLine("----------------------------");
                 Console.WriteLine("  SUPERVIVENCIA EN LA ISLA");
                 Console.WriteLine("----------------------------");
                 Console.WriteLine("Dias sobrevividos: " + Dias);
                 Console.WriteLine("Vida: " + Vida + "  Hambre: " + Hambre);
-                if (Comida_Cruda > 0)
-                {
-                    Console.WriteLine("Comida Cruda" + Comida_Cruda);
-                }
-                if (Comida_Cocida > 0)
-                {
-                    Console.WriteLine("Comida" + Comida_Cocida);
-                }
-                if (Refugio == true && Fogata == true)
-                {
-                    Console.WriteLine("Posees un refugio y una fogata");
-                }
-                else if (Refugio == true && Fogata == false)
-                {
-                    Console.WriteLine("Posees un refugio.");
-                }
-                else if (Refugio == false && Fogata == true)
-                {
-                    Console.WriteLine("Posees una fogata");
-                }
+                Console.WriteLine("Comida Cruda: " + Comida_Cruda + "  Madera: " + Madera);
+                Console.WriteLine("Comida: " + Comida_Cocida);
                 Console.WriteLine("----------------------------");
                 Console.WriteLine("> Buscar comida");
                 Console.WriteLine("> Explorar la isla");
@@ -57,22 +45,144 @@ namespace _18_SimuladorJuego
                 Console.Write("> ");
                 Accion = Console.ReadLine();
                 switch (Accion.ToLower())
+
                 {
+                    default:
+                        Console.Clear();
+                        break;
                     case "buscar comida":
-                        int Rand_o = NumR.Next(1, 100);
-                        
+                        if (Num >= 50)
+                        {
+                            Console.WriteLine("> Encontraste comida! (+2 Comida cruda)");
+                            Comida_Cruda += 2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("> No encontraste nada.");
+                        }
+                        Console.ReadKey();
+                        Vida -= 1;
+                        Console.Clear();
                         break;
                     case "explorar la isla":
+                        if (Num >= 50)
+                        {
+                                Console.WriteLine("> encontraste madera.");
+                                Madera += 1;
+                        }
+                        else if (Num <= 49 && Num >= 21)
+                        {
+                            Console.WriteLine("> No encontraste nada.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("> Sufriste un accidente (-2 vida)");
+                            Vida -= 2;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                     case "construir refugio":
+                        if (Madera == 0)
+                        {
+                            Console.WriteLine("> Te quedaste pensando todo el dia en hacer tu refugio");
+                            Console.WriteLine("> aunque no tenias el material necesario.");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        else if (Madera > 0)
+                        {
+                            Console.WriteLine("> Construiste un refugio");
+                            Refugio = true;
+                            Madera -= 1;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                     case "encender fogata":
+                        if (Madera == 0)
+                        {
+                            Console.CursorTop--;
+                            Console.Write(new string(' ', Console.WindowWidth));
+                            Console.CursorTop--;
+                            Console.Clear();
+                            Hambre += 2;
+                            Dias += 1;
+                        }
+                        else if (Madera > 0)
+                        {
+                            Console.WriteLine("> Hiciste una fogata");
+                            Fogata = true;
+                            Madera -= 1;
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
                         break;
                     case "cocinar comida cruda":
+                        if (Fogata == false && Comida_Cruda == 0)
+                        {
+                            Console.WriteLine("> Te quedaste pensando en olor de la comida que cocinarias");
+                            Console.WriteLine("> pero no cocinaste nada..");
+                        }
+                        else if (Comida_Cruda > 0)
+                        {
+                            Console.WriteLine("> Cocinaste comida");
+                            Comida_Cruda -= 1;
+                            Comida_Cocida += 1;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                     case "comer comida":
+                        if (Comida_Cruda == 0 && Comida_Cocida == 0)
+                        {
+                            Console.WriteLine("> Comiste aire");
+                            Console.ReadKey();
+                        }
+                        else if (Comida_Cruda > 1 && Comida_Cocida == 0)
+                        {
+                            Console.WriteLine("> Comiste comida cruda, Baboso");
+                            Comida_Cruda -= 1;
+                            Hambre += 1;
+                            Vida -= 1;
+                        }
+                        else if (Comida_Cocida > 0)
+                        {
+                            Console.WriteLine("> Comiste comida");
+                            Comida_Cocida -= 1;
+                            Hambre += 4;
+                        }
+
+                        if (Hambre > 10)
+                        {
+                            Hambre = 10;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                     case "descansar":
+                        if (Refugio == true && Fogata == true)
+                        {
+                            Console.WriteLine("> Dormiste excelente");
+                            Vida += 8;
+                        }
+                        else if (Refugio == true || Fogata == true)
+                        {
+                            Console.WriteLine("> Dormiste bien");
+                            Vida += 4;
+                        }
+                        else
+                        {
+                            Console.WriteLine("> Dormiste");
+                            Vida += 1;
+                        }
+
+                        if (Vida > 10)
+                        {
+                            Vida = 10;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                     case "salir":
                         {
@@ -95,9 +205,19 @@ namespace _18_SimuladorJuego
                             }
                             Console.Clear();
                             break;
+                    
                         }
-
                 }
+                if (Hambre > 0)
+                {
+                    Hambre -= 2;
+                }
+                else
+                {
+                    Vida -= 2;
+                }
+                Dias += 1;
+                
             }
         }
     }
---
+++ Etapa2/19_BlackJack/19_BlackJack/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int P_Jugador = 0; int P_Crupier = 0;
+            string opcion = ""; bool Planto = false;
+            Random Nram = new Random();
+            while (true) 
+            {
+                Nram.Next(1, 101);
+                Console.WriteLine("===== BLACKJACK =====");
+                Console.WriteLine();
+                Console.WriteLine("Puntaje Jugador:" + P_Jugador);
+                Console.WriteLine("Puntaje Crupier: Oculto");
+                Console.WriteLine();
+                Console.WriteLine("1. Pedir carta");
+                Console.WriteLine("2. Plantarse");
+                Console.WriteLine("3. Ver reglas");
+                Console.WriteLine("4. Salir");
+                Console.WriteLine();
+                Console.WriteLine("Escriba que hacer");
+                opcion = Console.ReadLine();
+                switch (opcion.ToLower())
+                {
+                    case "pedir carta":
+                        break;
+                    case "plantarse":
+                        break;
+                    case "ver reglas":
+                        Console.Clear();
+                        Console.WriteLine("⌌-----------------------------------------------------------------------------------------");
+                        Console.WriteLine("│                         <==--- REGLAS ---==>");
+                        Console.WriteLine("│· El jugador compite contra la computadora, que actúa como crupier.");
+                        Console.WriteLine("│· El objetivo es acercarse lo más posible a 21 puntos sin pasarse.");
+                        Console.WriteLine("│· Cada carta suma puntos al puntaje total del jugador o del crupier.");
+                        Console.WriteLine("│· Si el jugador supera los 21 puntos, pierde la partida automáticamente.");
+                        Console.WriteLine("│· Si el jugador decide plantarse, deja de pedir cartas y comienza el turno del crupier.");
+                        Console.WriteLine("│· El crupier debe pedir cartas automáticamente mientras tenga menos de 17 puntos.");
+                        Console.WriteLine("│· Cuando el crupier llega a 17 puntos o más, se planta.");
+                        Console.WriteLine("│· Si el crupier supera los 21 puntos, gana el jugador.");
+                        Console.WriteLine("│· Si ninguno se pasa de 21, gana quien tenga el puntaje más alto.");
+                        Console.WriteLine("│· Si ambos terminan con el mismo puntaje, la partida queda empatada.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "salir":
+                        break;
+
+                }
+            }
+        }
+    }
+}
